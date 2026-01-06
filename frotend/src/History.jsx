7403d30afb1d60707f31eb0cecdde2ba12ad32a8
Gabytzu3135 <90730221+Gabytzu3135@users.noreply.github.com>
2026-01-06 17:05:18 +0200

    Update History.jsx

--- frotend/src/History.jsx
+++ frotend/src/History.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
+import { ClipLoader } from "react-spinners";
+import MapIcon from "./map.svg?react";
 
 const History = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({
+    stats: { total: 0, confirmed: 0, lastTime: "-" },
+    logs: [],
+    activeAlert: null,
+    isOnline: false,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://infoap-production.up.railway.app/api/data")
+        .then((res) => res.json())
+        .then((payload) => {
+          setData(payload);
+          setLoading(false);
+        })
+        .catch(err);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading)
+    return (
+      <>
+        <Sidebar />
+        <div className="fixed inset-0 flex flex-col justify-center items-center z-50 lg:ml-40">
+          <ClipLoader size={60} color="#000" />
+          <p className="mt-4 font-medium animate-pulse tracking-wide text-black">
+            Checking ESP32 System..
+          </p>
+        </div>
+      </>
+    );
   return (
     <>
       <Sidebar />
@@ -9,39 +45,23 @@ const History = () => {
         <h2 className="title text-3xl">Alert Logs</h2>
       </div>
       <div className="lg:ml-60 flex-col-reverse justfiy-start mt-10">
-        <div className="inline-flex border-b border-[#dbdbdb] w-full gap-10 h-20 justify-center items-center">
-          <p>
-            Location: <b>Brasov, Romania</b>
-          </p>
-          <p>
-            Date: <b>03/01/2026 11:40 PM</b>
-          </p>
-          <a href="" className="text-blue-400">
-            View on map
-          </a>
-        </div>
-        <div className="inline-flex border-b border-[#dbdbdb] w-full gap-10 h-20 justify-center items-center">
-          <p>
-            Location: <b>Brasov, Romania</b>
-          </p>
-          <p>
-            Date: <b>03/01/2026 11:40 PM</b>
-          </p>
-          <a href="" className="text-blue-400">
-            View on map
-          </a>
-        </div>
-        <div className="inline-flex border-b border-[#dbdbdb] w-full gap-10 h-20 justify-center items-center">
-          <p>
-            Location: <b>Brasov, Romania</b>
-          </p>
-          <p>
-            Date: <b>03/01/2026 11:40 PM</b>
-          </p>
-          <a href="" className="text-blue-400">
-            View on map
-          </a>
-        </div>
+        {data?.logs.map((data, i) => (
+          <div className="inline-flex border-b border-[#dbdbdb] w-full gap-10 h-20 justify-center items-center hover:bg-slate-50 transition-colors duration-200">
+            <p>
+              Location: <b>{data.location}</b>
+            </p>
+            <p>
+              Date: <b>{data.date}</b>
+            </p>
+            <a
+              href="https://www.google.com/maps?q=45.6443,25.5956"
+              target="_blank"
+              className="text-blue-400 flex items-center gap-3  "
+            >
+              <MapIcon className="h-8 w-8" />
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
