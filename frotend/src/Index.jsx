ecf5979de8c5e7b5e0363890192f6779a436c658
Gabytzu3135 <90730221+Gabytzu3135@users.noreply.github.com>
2026-01-08 16:43:20 +0200

    Update Index.jsx

--- frotend/src/Index.jsx
+++ frotend/src/Index.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import { ClipLoader } from "react-spinners";
 import MapIcon from "./map.svg?react";
+import ClockIcon from "./time.svg?react";
+import CoordIcon from "./coord.svg?react";
 import ConfirmIcon from "./confirm.svg?react";
+import AlertIcon from "./alert.svg?react";
 import Swal from "sweetalert2";
 
 function App() {
@@ -30,29 +33,35 @@ function App() {
   }, []);
 
   const handleConfirm = () => {
-    Swal.fire({
-      title: "You are about to confirm an SOS call.",
-      text: "Before proceeding, please verify the alert location and take all necessary steps to provide assistance.",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#1d6e1e",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Send confirmation",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch("https://infoap-production.up.railway.app/api/confirm", {
-          method: "POST",
-        })
-          .then(() => console.log("Alertă confirmată"))
-          .catch((err) => console.error(err));
+    if (data.isOnline) {
+      Swal.fire({
+        title: "You are about to confirm an SOS call.",
+        text: "Before proceeding, please verify the alert location and take all necessary steps to provide assistance.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#1d6e1e",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Send confirmation",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch("https://infoap-production.up.railway.app/api/confirm", {
+            method: "POST",
+          }).catch((err) => console.error(err));
 
-        Swal.fire({
-          title: "Confirmed!",
-          text: "Confirmation signal sent to the ESP32 board. Please follow all necessary procedures to provide assistance. You can always review the location in the Alerts History tab.",
-          icon: "success",
-        });
-      }
-    });
+          Swal.fire({
+            title: "Confirmed!",
+            text: "Confirmation signal sent to the ESP32 board. Please follow all necessary procedures to provide assistance. You can always review the location in the Alerts History tab.",
+            icon: "success",
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "The board is disconnected.",
+      });
+    }
   };
   if (loading)
     return (
@@ -91,41 +100,54 @@ function App() {
         <h1 className="title text-3xl">Alert Information</h1>
       </div>
 
-      {!data.activeAlert || !data.isOnline ? (
+      {!data.activeAlert ? (
         <div className="mt-10 border border-[#dbdbdb] flex items-center justify-start lg:ml-60 rounded-2xl text-left text-sm p-5 font-bold h-20 hover:shadow-md transition-all duration-300 shadow-sm">
           <h1 className="text-left text-md">No active alerts at the moment.</h1>
         </div>
-      ) : (data.isOnline &&
-        <div className="mt-10 border border-[#dbdbdb] flex-col lg:ml-60 rounded-2xl  text-sm font-bold h-auto pb-5 shadow-sm hover:shadow-lg transition-all duration-300">
-          <div className="flex border-b border-[#dbdbdb] h-15 p-2 rounded-t-xl justify-start items-center bg-orange-600">
-            <h1 className="text-2xl p-5 text-white title ">
-              <span className="text-white title tracking-[.05em]">
-                Warning! New alert!
-              </span>{" "}
+      ) : (
+        <div className="mt-10 border border-[#dbdbdb] flex-col lg:ml-60 rounded-2xl  text-sm font-bold h-auto pb-5 shadow-sm hover:shadow-lg transition-all duration-300 ">
+          <div className="flex border-b border-[#dbdbdb] h-15 p-2 rounded-t-xl justify-start items-center bg-red-500 shadow-md">
+            <h1 className="text-md p-5 text-white title flex items-center space-x-3">
+              <AlertIcon className="h-5 w-5" />
+              <span className="text-white">Alert in progress</span>{" "}
             </h1>
           </div>
-          <div className="flex flex-col space-y-3 p-5">
-            <p className="font-normal text-center">
-              New alert located in <b>{data.activeAlert.location}</b> at
-              coordinates 45.6443° N, 25.5956° E
-              <br />
-              Received at: <b>{data.activeAlert.time}</b>
+          <div className="flex flex-col space-y-3 p-5 ">
+            <p className="flex items-center space-x-3">
+              <MapIcon className="h-5 w-5 " />
+              <span className="font-normal ">
+                Alert location: <b>{data.activeAlert.location}</b>
+              </span>
             </p>
-
+            <p className="flex items-center space-x-3">
+              <CoordIcon className="h-5 w-5 " />
+              <span className="font-normal ">
+                Coordinates: <b> 45.6443° N, 25.5956° E</b>
+              </span>
+            </p>
+            <p className="flex items-center space-x-3">
+              <ClockIcon className="h-5 w-5 text-black" />
+              <span className="font-normal">
+                Requested at: <b>{data.activeAlert.time}</b>
+              </span>
+            </p>
+            <br />
             <a
               href="https://www.google.com/maps?q=45.6443,25.5956"
               target="_blank"
-              className="bg-blue-700  h-10 rounded-xl cursor-pointer text-center p-2 hover:shadow-xl hover:scale-[1.01] hover:bg-blue-600 transition-all duration-300 text-white flex justify-center items-center gap-2"
+              className="bg-blue-600  h-10 rounded-xl cursor-pointer text-center p-2 hover:shadow-xl hover:scale-[1.01] hover:bg-blue-500 transition-all duration-300 text-white flex justify-center items-center gap-2"
             >
               <MapIcon className="h-4 w-4" />
               <span>View on map</span>
             </a>
             <button
               onClick={handleConfirm}
-              className="bg-green-600 h-10 rounded-xl cursor-pointer hover:shadow-xl hover:scale-[1.01] hover:bg-green-500  transition-all duration-300 text-white flex justify-center items-center gap-2"
+              className={`bg-green-600 h-10 rounded-xl hover:shadow-xl hover:scale-[1.01] hover:bg-green-500  transition-all duration-300 text-white flex justify-center items-center gap-2 cursor-pointer ${
+                !data.isOnline && "bg-red-300 hover:bg-red-300"
+              } `}
             >
               <ConfirmIcon className="h-4 w-4" />
-              <p>Confirm - Help sent</p>
+              <p>Confirm - Send help</p>
             </button>
           </div>
         </div>
